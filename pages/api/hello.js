40613e112e92932edf5b87bd79fef5fb29407b95
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-02-15 14:50:07 +0000

    added star wars api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello' })
-}
+export default async (req, res) => {
+  // Instead of static content this would be the place for a db request.
+  const data = {
+    title: "This is a title",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+  };
+
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
+  res.end(JSON.stringify({ message: "success", data }));
+};
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import Date from '../components/date'
-import { getSortedPostsData } from '../lib/posts'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
+  const request = await fetch("https://www.swapi.tech/api/people/1");
+  const json = await request.json();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+      json: json.result,
+    },
+  };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, json }) {
   return (
     <Layout home>
       <Head>
@@ -23,7 +26,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -32,17 +35,18 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
+      <p>{json.description}</p>
     </Layout>
-  )
+  );
 }
